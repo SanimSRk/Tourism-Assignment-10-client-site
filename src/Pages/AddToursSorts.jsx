27f1fafdf670a382761da041310f6511ad20a589
SanimSRk <hasansanim562@gmail.com>
2024-04-26 07:12:19 +0600

    add porduct form deging done

--- src/Pages/AddToursSorts.jsx
+++ src/Pages/AddToursSorts.jsx
@@ -1,7 +1,147 @@
+import { useForm } from 'react-hook-form';
+
 const AddToursSorts = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
   return (
     <div>
-      <h2>This is add form tourish</h2>
+      <div className="w-2/3 mx-auto my-[100px] shadow-lg py-[72px] px-[100px]">
+        <h2 className="text-3xl font-bold text-center mt-3">
+          <span className="text-[#FF497C]">Add</span> Your Product
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex gap-4">
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Tourists Spot Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Tourists_spot_name"
+                className="input input-bordered"
+                required
+                {...register('TouristsSoprt', { required: true })}
+              />
+            </div>
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Country Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter country_Name"
+                className="input input-bordered"
+                required
+                {...register('countryName', { required: true })}
+              />
+            </div>
+          </div>
+          <div className="flex gap-4 my-2">
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Location</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your brand name "
+                className="input input-bordered"
+                required
+                {...register('location', { required: true })}
+              />
+            </div>
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Short description </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Short description"
+                className="input input-bordered"
+                required
+                {...register('description', { required: true })}
+              />
+            </div>
+          </div>
+          <div className="flex gap-4 my-2">
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Average cost</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter average_cost "
+                className="input input-bordered"
+                required
+                {...register('averageCost', { required: true })}
+              />
+            </div>
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Seasonality </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter seasonality "
+                className="input input-bordered"
+                required
+                {...register('seasonality ', { required: true })}
+              />
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">Travel time</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter travel_time"
+                className="input input-bordered"
+                required
+                {...register('travelTime', { required: true })}
+              />
+            </div>
+            <div className="form-control w-1/2">
+              <label className="label">
+                <span className="label-text">TotaVisitorsPerYea</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter totaVisitorsPerYea"
+                className="input input-bordered"
+                required
+                {...register('totaVisitorsPerYea', { required: true })}
+              />
+            </div>
+          </div>
+
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text"> Image Url </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter image url......."
+              className="input input-bordered"
+              required
+              {...register('image', { required: true })}
+            />
+          </div>
+          <input
+            className="text-white btn w-full mt-6 bg-[#FF497C]"
+            type="submit"
+            value="Add Product"
+          />
+        </form>
+      </div>
     </div>
   );
 };
